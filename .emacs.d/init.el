1f4c8b8cdbe3923a7d5d1125ee59aa32a14ae7c5
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-05-18 14:24:40 -0400

    Create init.el.

---
+++ .emacs.d/init.el
@@ -0,0 +1,39 @@
+(setq package-enable-at-startup nil) ;; Prevent early package loading
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'exec-path-from-shell)
+  (package-refresh-contents)
+  (package-install 'exec-path-from-shell))
+
+;; Ensure these packages are installed
+(setq package-selected-packages '(company go-mode lsp-mode markdown-mode))
+(dolist (pkg package-selected-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+(when (memq window-system '(mac ns x))
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-initialize))
+
+
+;; Setup Go with LSP
+(require 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company exec-path-from-shell go-mode lsp-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
