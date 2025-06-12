642c9aba3b9f3e11074f4ce5a409b591d1c99b39
xtt28 <155261788+xtt28@users.noreply.github.com>
2025-06-12 18:54:24 -0400

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
   (package-install 'exec-path-from-shell))
 
 ;; Ensure these packages are installed
-(setq package-selected-packages '(company go-mode lsp-mode markdown-mode))
+(setq package-selected-packages '(company go-mode lsp-mode markdown-mode catppuccin-theme))
 (dolist (pkg package-selected-packages)
   (unless (package-installed-p pkg)
     (package-install pkg)))
@@ -24,13 +24,17 @@
 ;; Setup Go with LSP
 (require 'lsp-mode)
 (add-hook 'go-mode-hook #'lsp)
+(add-hook 'c-mode-hook #'lsp)
+
+;; Setup theme
+(load-theme 'catppuccin :no-confirm)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company exec-path-from-shell go-mode lsp-mode)))
+ '(package-selected-packages '(treemacs company exec-path-from-shell go-mode lsp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
